2eb40a9c7e21994d7b198fcdc03f256509d17223
Anji@515 <anji.gd17@gmail.com>
2023-09-17 18:20:39 +0530

    Developed signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import SignUp from './Components/Signup';
 import AuthDetails from './Components/AuthDetails';
+import SignIn from './Components/Signin';
 
 function App() {
   return (
     <div className="App">
+      <h1>Firebase Authentication</h1>
+      <br/>
       <SignUp/>
+      <SignIn/>
       <AuthDetails/>
     </div>
   );
